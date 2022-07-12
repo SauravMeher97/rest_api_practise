ae0a653bc8ab2d6b9c47e1f643ef18ee8968c8d9
Saurav Meher <saurav.m7742@gmail.com>
2022-07-12 16:21:38 +0530

    Testing basic controller (REST)

---
+++ src/main/java/com/restapiexample/restcontroller/FirstexampleApplication.java
@@ -0,0 +1,16 @@
+package com.restapiexample.restcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstexampleApplication.class, args);
+		
+		System.out.println("Hello Spring");
+		
+	}
+
+}
---
+++ src/main/java/com/restapiexample/restcontroller/controller/testcontroller.java
@@ -0,0 +1,19 @@
+package com.restapiexample.restcontroller.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testcontroller {
+	
+	
+	@Value("${message}")
+	String message;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return message;
+	}
+
+}
